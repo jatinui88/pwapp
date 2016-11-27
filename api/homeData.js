f2b8c50148a228bf8db49729ca25cd2ca4080b09
jatinui88 <jatin.ui88@gmail.com>
2016-11-27 19:14:51 +0530

    Add files via upload

---
+++ api/homeData.js
@@ -0,0 +1,17 @@
+var carouselArr = [
+                   {
+                    "url":"https://mdbootstrap.com/images/ecommerce/slides/slide%20(1).jpg",
+                    "title":"New collection",
+                    "alt" : "First Slide"
+                   },
+                   {
+                    "url":"https://mdbootstrap.com/images/ecommerce/slides/slide%20(2).jpg",
+                    "title":"Get discount!",
+                    "alt" : "First Slide"
+                   },
+                   {
+                     "url":"https://mdbootstrap.com/images/ecommerce/slides/slide%20(3).jpg",
+                     "title":"Only now for 10$",
+                     "alt" : "First Slide"
+                   }
+];
